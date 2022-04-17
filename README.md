ef9be453c7f5d271ec51e311db08273e0315fa25
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-17 06:46:18 +0530

    Understanding JSX

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+Tis is just a react app tut from scratch
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 
+let name = 'World';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <img src="" alt="" />
+      <div className="container">
+        <h1>Hello {name}</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+      </div>
+    </>
   );
 }
 
