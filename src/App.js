1ff2f6f19c3d06d7d970b9279060acb23bd7ac92
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-18 09:10:17 +0530

    routes & routers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
 
 function App() {
   return (
-    <>      
-      <Navbar about="About"/>
-      <div className="container">
-        {/* <TextForm heading="Enter text to analyze"/>    */}
-        <About/>      
-      </div>   
-        
+    <>           
+      <Router>
+        <Navbar about="About"/>
+          <div className="container">
+            <Routes>          
+              <Route path="/" element={<TextForm heading="Enter text to analyze"/>} />             
+              <Route path="/about" element={<About/>} />
+            </Routes> 
+          </div>
+      </Router>      
     </>
   );
 }
